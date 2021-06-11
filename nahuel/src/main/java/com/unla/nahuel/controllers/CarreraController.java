dd6e9e239848f1ae6489a37a394822fff0fff0bf
nahu3l62 <nahuelruiz20@gmail.com>
2021-06-10 23:20:27 -0300

    final

--- nahuel/src/main/java/com/unla/nahuel/controllers/CarreraController.java
+++ nahuel/src/main/java/com/unla/nahuel/controllers/CarreraController.java
@@ -1,29 +1,30 @@
 package com.unla.nahuel.controllers;
 
+
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.unla.nahuel.entities.Carreras;
-import com.unla.nahuel.entities.Perfiles;
-import com.unla.nahuel.entities.Usuario;
 import com.unla.nahuel.helpers.ViewRouteHelper;
 import com.unla.nahuel.services.ICarreraService;
 
@@ -85,16 +86,40 @@ public class CarreraController {
 		return "materias/index";
 	}
 	
-	@GetMapping("/materia/proyecto")
-	public String proyecto(Model model) {
+	private static String UPLOADED_FOLDER = "C://archivos//";
 	
-		model.addAttribute("titulo", "Licenciatura en sistemas");
-
+	@GetMapping("/materia/proyecto")
+	public String proyecto(Model model) throws IOException{
+		
+		
+	
+            List<File> files = Files.list(Paths.get(UPLOADED_FOLDER))
+                        .map(Path::toFile)
+                        .collect(Collectors.toList());
+             
+           
+           Iterator<File> iterador = files.iterator();
+           
+           File file = new File(UPLOADED_FOLDER + "/" + "temperaturas.txt");
+           while(iterador.hasNext()) {
+        	   File prueba = iterador.next();
+        	   System.out.println(prueba.getName());
+           }
+           
+           //System.out.println(files.get(0).getName());
+           model.addAttribute("titulo", "Licenciatura en sistemas");
+           model.addAttribute("files", files);
+           model.addAttribute("filess", file);
+		
+		//System.out.println(files);
+		
+		
+		
 		return "materias/proyecto";
 	}
 	
 	
-	@RequestMapping(value = "/uploadFile", method = RequestMethod.GET)
+	/*@RequestMapping(value = "/uploadFile", method = RequestMethod.GET)
 	public String submit(@RequestParam("file") File file, Model model) {
 		
 	    model.addAttribute("file", file.getName());
@@ -102,8 +127,45 @@ public class CarreraController {
 	    
 	    System.out.println(file.getName());
 	    return "materias/subida";
-	}
+	}*/
 	
+	
+	
+	
+	@PostMapping("/upload")
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes, Model model) throws IOException {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Por favor seleccione un archivo a subir.");
+            return "redirect:uploadStatus";
+        }
+
+        try {
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "Se completo la subida del archivo '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+       
+        
+        
+        return "redirect:/carreras/uploadStatus";
+    }
+
+    @GetMapping("/uploadStatus")
+    public String uploadStatus() {
+        return "/materias/status";
+    }
+	
+
+
 	
 	
 	
